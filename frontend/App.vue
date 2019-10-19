cc0b77e1d99d2b49c37c672470fbd3c96e265b43
FeLL1kS <fell1ks@wbydcloud.com>
2019-10-19 15:34:49 +0300

    Include Vue.JS

---
+++ frontend/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="root">
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="css">
+    #root {
+        width: 100%;
+        background: #000;
+        background-repeat: repeat;
+    }
+</style>
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,15 +1,38 @@
+const {
+  VueLoaderPlugin
+} = require("vue-loader");
+
 module.exports = {
-    context: __dirname,
-    devtool: "source-map",
-    entry: "./index.js",
-    output: {
-      path: __dirname + "/../backend/public",
-      filename: "app.js"
-      
-    },
-    module:{
-        loaders: [
-          {test : /\.css$/, loader: 'style!css!'}
-        ]
-    }
+  context: __dirname,
+  devtool: "source-map",
+  entry: "./main.js",
+  output: {
+    path: __dirname + "/../backend/public",
+    filename: "app.js"
+
+  },
+  plugins: [new VueLoaderPlugin()],
+  module: {
+
+    rules: [{
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            ts: 'ts-loader'
+          },
+          esModule: true
+        },
+
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+        loader: 'file-loader'
+      },
+    ]
   }
+};
