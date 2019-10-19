6d9489533c59d116bad578162e402b07f65d94e4
FeLL1kS <fell1ks@wbydcloud.com>
2019-10-20 00:00:50 +0300

    AAAAAAAAAAAAAAAAA

--- frontend/App.vue
+++ frontend/App.vue
@@ -1,72 +1,75 @@
 <template>
     <div id="root">
         <canvas id="star-sky"></canvas>
+        <navbar />
         <h1>App</h1>
 	    <router-view></router-view>
     </div>
 </template>
 
 <script>
-export default {
-    name: 'App',
-    mounted: () => {
-      var c = document.getElementById('star-sky');
-      var ctx = c.getContext('2d');
-      var x = c.width = window.innerWidth;
-      var y = c.height = window.innerHeight;
-      var nStar = Math.round((x + y) / 5);
-      var randomSize = Math.floor((Math.random() * 2) + 1);
+    import Navbar from './app/components/NavBar.vue';
 
-      var stars = [];
+    export default {
+        name: 'App',
+        components: { 
+            Navbar 
+        },
+        mounted: () => {
+        var c = document.getElementById('star-sky');
+        var ctx = c.getContext('2d');
+        var x = c.width = window.innerWidth;
+        var y = c.height = window.innerHeight;
+        var nStar = Math.round((x + y) / 5);
+        var randomSize = Math.floor((Math.random() * 2) + 1);
 
-      var fps = 0;
-      setInterval(function() {
-        fps = 0;
-      }, 1000);
+        var stars = [];
 
-      function createStars() {
-        'use strict';
-        stars = [];
-        for (var i = 0; i <= nStar; i++) {
-          stars.push({
-            x: Math.random() * x,
-            y: Math.random() * y,
-            o: Math.random(),
-            r: Math.random(),
-            s: 0.02,
-          })
-          if (randomSize > .1) {
-            ctx.shadowBlur = Math.floor((Math.random() * 15));
-            ctx.shadowColor = "white";
-          }
+        var fps = 0;
+        setInterval(function() {
+            fps = 0;
+        }, 1000);
+
+        function createStars() {
+            'use strict';
+            stars = [];
+            for (var i = 0; i <= nStar; i++) {
+            stars.push({
+                x: Math.random() * x,
+                y: Math.random() * y,
+                o: Math.random(),
+                r: Math.random(),
+                s: 0.02,
+            })
+            if (randomSize > .1) {
+                ctx.shadowBlur = Math.floor((Math.random() * 15));
+                ctx.shadowColor = "white";
+            }
+            }
         }
-      }
 
-      function drawing() {
-        'use strict';
-        ctx.clearRect(0, 0, innerWidth, innerHeight);
-        fps++;
-        for (var i = 0; i <= nStar; i++) {
-          var e = stars[i];
-          if (e.o > 1 || e.o < 0) {
-            e.s = -e.s;
-          }
-          e.o += e.s;
-          ctx.beginPath();
-          ctx.arc(e.x, e.y, e.r, 0, Math.PI * 2, false);
-          ctx.strokeStyle = 'rgba(255, 255, 255, ' + e.o + ')';
-          ctx.stroke();
+        function drawing() {
+            'use strict';
+            ctx.clearRect(0, 0, innerWidth, innerHeight);
+            fps++;
+            for (var i = 0; i <= nStar; i++) {
+            var e = stars[i];
+            if (e.o > 1 || e.o < 0) {
+                e.s = -e.s;
+            }
+            e.o += e.s;
+            ctx.beginPath();
+            ctx.arc(e.x, e.y, e.r, 0, Math.PI * 2, false);
+            ctx.strokeStyle = 'rgba(255, 255, 255, ' + e.o + ')';
+            ctx.stroke();
+            }
+            requestAnimationFrame(drawing);
         }
-        requestAnimationFrame(drawing);
-      }
 
-      createStars();
-      drawing();
+        createStars();
+        drawing();
+        }
     }
-}
-
-
-
 </script>
 
 <style lang="css" scoped>
---
+++ frontend/app/components/NavBar.vue
@@ -0,0 +1,129 @@
+<template>
+    <nav class="nav">
+        <input class="nav__check" type="checkbox" id="showmenu"/>
+        <label class="nav__showmenu" for="showmenu">&#9776;</label>
+        <!-- <div class="top"> -->
+            <ul class="menu">
+                <li>
+                    <a class="menu__item" href="#">Systems</a>
+                    <input class="menu__check" type="checkbox" id="menu2">
+                    <label class="menu__showsub" for="menu2">&#9660;</label>
+                    <ul class="menu__submenu">
+                        <li v-for="star in stars"><a class="menu__item" href="#">{{star.name}}</a></li>
+                    </ul>
+                </li>
+            </ul>
+        <!-- </div> -->
+        <div class="bot">
+            <ul class="menu">
+                <li><a href="#" class="menu__item">Profile</a></li>
+                <li><a href="#" class="menu__item">LogOut</a></li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "navbar",
+    data: () => {
+        return {
+            stars: [
+                {
+                    name: "Sun",
+                    id: "001"
+                },
+                {
+                    name: "Betelgeize",
+                    id: "002"
+                }
+            ]
+        } 
+    }
+}
+</script>
+
+<style scoped>
+    input {display: none;}
+    label {cursor: pointer;}
+
+    .nav {user-select: none;}
+
+    .menu>li:first-child {
+        margin-top: 2vh;
+    }
+
+    .nav__showmenu {
+        position: relative;
+        z-index: 3;
+        display: inline-block;
+        margin-left: -10px;
+        padding: 5px 10px 10px;
+        top: 2px;
+        font-size: 23px;
+        left: 15px;
+    }
+
+    .menu, .menu__submenu{
+        list-style: none;
+        padding: 0;
+        color: #fff;
+        overflow: hidden;
+    }
+
+    .menu {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        display: block;
+        width: 220px;
+        margin: 0;
+        padding: 17px 0;
+        text-transform: uppercase;
+        line-height: 2em;
+        background-color: #2c2c2c;
+        overflow-y: auto;
+        -webkit-overflow-scrolling: touch;
+        transform: translateX(-100%);
+        transition: transform .3s;
+        will-change: transform;
+        backface-visibility: hidden;
+    }
+
+    .menu__submenu {
+        font-size: .8em;
+        background-color: #ddd;
+        max-height: 0;
+        transition: max-height .3s ease-in-out;
+    }
+
+    .menu__showsub {
+        position: absolute;
+        right: 0;
+        margin-top: -34px;
+        padding: 10px;
+        line-height: normal;
+    }
+
+    .menu__item {  
+        display: block;
+        text-align: center;
+        text-decoration: none;
+        color: #fff;
+    }
+
+    .menu__submenu .menu__item {color: #000;}
+
+    .nav__check:checked~.nav__back {
+        background-color: rgba(0,0,0,.2);
+        bottom: 0;
+    }
+
+    .menu__item:hover,.nav__showmenu:hover,.menu__showsub:hover {opacity: 0.7;}
+    .menu::-webkit-scrollbar {display: none;}
+    .nav__check:checked+.nav__showmenu {color:#fff;}
+    .menu__check:checked~.menu__submenu {max-height: 100vh;}
+    .menu__check:checked+.menu__showsub {transform: rotate(180deg);}
+    .nav__check:checked~.menu {transform: translateX(0);}
+</style>
