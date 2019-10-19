59a17cc1c5fff40653d03fb077e66e360e48481d
FeLL1kS <fell1ks@wbydcloud.com>
2019-10-20 02:10:46 +0300

    system

---
+++ frontend/app/components/System.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="system">
+        <ul class="starSystem">
+            <li class="star">
+                <a href="#star">
+                    <span> {{ star.name }} </span>
+                </a>
+            </li>
+            <li v-for="planet in planets" class={{ planet.name }}>
+                <a href="#">
+                    <span>
+                        {{ planet.name }}
+                    </span>
+                </a>
+            <li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "system",
+    mounted: () => {
+        
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
